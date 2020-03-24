ae3d711757b6be24c85830843d30291032c8fe57
andresaugusto-ga <andres.augusto.ga@icloud.com>
2020-03-23 20:05:10 -0400

    Added Shuffle Function and debugged same card being pushed twice to cardsInPlay array

--- memory_game/memory_game/js/main.js
+++ memory_game/memory_game/js/main.js
@@ -30,27 +30,49 @@ console.log("Finally! Up and running!");
 
 const cards = [
 	{
-	rank: "queen",
+	rank: "1",
 	suit: "hearts",
 	cardImage: "images/queen-of-hearts.png",
 	},
 	{
-	rank: "king",
+	rank: 1,
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+	rank: "2",
 	suit: "hearts",
 	cardImage: "images/king-of-hearts.png",
 	},
 	{
-	rank: "king",
+	rank: 2,
 	suit: "diamonds",
 	cardImage: "images/king-of-diamonds.png",
 	},
-	{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png",
-	},
 ];
 
+// Fisher-Yates Shuffle adaptation
+function shuffle(array) {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
+shuffle(cards);
 
 const cardsInPlay = [];
 
@@ -62,15 +84,56 @@ function scoreBoard() {
 }
 
 
+// function checkForMatch() {
+// 	if (cardsInPlay[0] === cardsInPlay[1]) {
+// 		alert("Pick a different card, fucker!");
+// 	} else if (cardsInPlay[0] == cardsInPlay[1]) {
+// 		alert("You found a match!");
+// 		scoreBoard();
+// 	} else {
+// 		alert("Sorry, try again.");
+// 	}
+// }
+
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("Pick a different card!");
+		cardsInPlay.pop(cardsInPlay[1]);
+	} else if (cardsInPlay[0] == cardsInPlay[1]) {
 		alert("You found a match!");
 		scoreBoard();
 	} else {
 		alert("Sorry, try again.");
+		// add some sort of .setAttribute line for FIRST TWO elements in cardsInPlay to show as "images/back.png" 
+		cardsInPlay.pop(cardsInPlay[0]);
+		cardsInPlay.pop(cardsInPlay[1]);
 	}
 }
 
+function checkForDuplicate() {
+	if ((cardsInPlay[2] === cardsInPlay[0]) || (cardsInPlay[2] === cardsInPlay[1])) {
+		alert("Pick a different card!");
+		cardsInPlay.pop(cardsInPlay[2]);
+	}
+}
+
+function checkForMatch1() {
+	if ((cardsInPlay[3] === cardsInPlay[0]) || (cardsInPlay[3] === cardsInPlay[1]) || (cardsInPlay[3] === cardsInPlay[2])) {
+		alert("Pick a different card!");
+		cardsInPlay.pop(cardsInPlay[3]);
+	} else if (cardsInPlay[2] === cardsInPlay[3]) {
+		alert("Pick a different card!");
+		cardsInPlay.pop(cardsInPlay[3]);
+	} else if (cardsInPlay[2] == cardsInPlay[3]) {
+		alert("You found a match!");
+		scoreBoard();
+	} else {
+		alert("Sorry, try again.");
+		// add some sort of .setAttribute line for LAST TWO elements in cardsInPlay to show as "images/back.png" 
+		cardsInPlay.pop(cardsInPlay[2]);
+		cardsInPlay.pop(cardsInPlay[3]);
+	}
+}
 
 function flipCard() {
 	var cardId = this.getAttribute('data-id');
@@ -79,9 +142,10 @@ function flipCard() {
 	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
-	}
-	if (cardsInPlay.length === 4) {
-		checkForMatch();
+	} else if (cardsInPlay.length === 3) {
+		checkForDuplicate();
+	} else if (cardsInPlay.length === 4) {
+		checkForMatch1();
 	}
 } 
 
@@ -101,6 +165,7 @@ function createBoard() {
 
 createBoard();
 
-
+var cardId = this.getAttribute('data-id');
+this.setAttribute('src', "images/back.png");
 
 
